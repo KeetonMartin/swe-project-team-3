a6d5ebf03a6f9cd8ecdb6d602d9e31f8efd73f7f
Will Harris-Braun <willharrisbraun@gmail.com>
2022-04-03 23:20:07 -0400

    Start adding Add Scholarship page to mobile app

--- scholarshipr/Scholarshipr/app/src/main/java/com/example/scholarshipr/ScholarshipData.java
+++ scholarshipr/Scholarshipr/app/src/main/java/com/example/scholarshipr/ScholarshipData.java
@@ -1,5 +1,10 @@
 package com.example.scholarshipr;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.example.scholarshipr.databinding.ActivityAddScholarshipBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class ScholarshipData {
@@ -10,5 +15,14 @@ public class ScholarshipData {
     public String getScholarshipName(){
         return KEY_Name;
     }
+
+
+    binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+            // should open AddScholarship activity
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    });
 
 }
