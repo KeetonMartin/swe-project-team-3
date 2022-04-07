eb87507de86394611009bb0fea6c2ad6fc03bf6b
Williamlaw718 <wlawrence@haverford.edu>
2022-04-06 21:21:08 -0400

    WoRK ON app

--- scholarshipr/Scholarshipr/app/src/main/java/com/example/scholarshipr/ScholarshipData.java
+++ scholarshipr/Scholarshipr/app/src/main/java/com/example/scholarshipr/ScholarshipData.java
@@ -1,14 +1,20 @@
 package com.example.scholarshipr;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ScholarshipData {
+
+public class ScholarshipData extends AppCompatActivity {
     public static final String KEY_Name = "ScholarshipName";
+    public static final String KEY_Info = "ScholarshipInfo";
     public static final int KEY_Amount = 0;
     public static final String KEY_Description = "Description";
 
     public String getScholarshipName(){
         return KEY_Name;
+    }
+    public String getScholarshipDescrip(){
+        return KEY_Info;
     }
 
 }
