6e5b8f5dd54c0c5001a083494ac82daaa85984bf
Williamlaw718 <wlawrence@haverford.edu>
2022-04-06 21:59:58 -0400

    Work on Connecting mobile to web

--- scholarshipr/Scholarshipr/app/src/main/java/com/example/scholarshipr/MainActivity.java
+++ scholarshipr/Scholarshipr/app/src/main/java/com/example/scholarshipr/MainActivity.java
@@ -8,9 +8,26 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
@@ -30,5 +47,51 @@ public class MainActivity extends AppCompatActivity {
         rvScholarships.setLayoutManager(new LinearLayoutManager(this));
 
 
+    }
+
+    public List<ScholarshipData> getAllScholarships() {
+        TextView tv = findViewById(R.id.statusField);
+
+        try {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute(() -> {
+                        try {
+                            // assumes that there is a server running on the AVD's host on port 3000
+                            // and that it has a /test endpoint that returns a JSON object with
+                            // a field called "message"
+
+                            URL url = new URL("http://10.0.2.2:3000/test");
+
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.connect();
+
+                            Scanner in = new Scanner(url.openStream());
+                            String response = in.nextLine();
+
+                            JSONObject jo = new JSONObject(response);
+
+                            // need to set the instance variable in the Activity object
+                            // because we cannot directly access the TextView from here
+                            message = jo.getString("message");
+
+                        } catch (Exception e) {
+                            message = e.toString();
+                        }
+                    }
+            );
+
+            // this waits for up to 2 seconds
+            // it's a bit of a hack because it's not truly asynchronous
+            // but it should be okay for our purposes (and is a lot easier)
+            executor.awaitTermination(2, TimeUnit.SECONDS);
+
+            // now we can set the status in the TextView
+            tv.setText(message);
+        } catch (Exception e) {
+            // uh oh
+            e.printStackTrace();
+            tv.setText(e.toString());
+        }
     }
 }
